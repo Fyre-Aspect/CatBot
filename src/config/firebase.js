2c3d1b456a63606b3ad92e0cc1fcbb0c5bb15945
Fyre-Aspect <aamirtinwala7@gmail.com>
2025-11-26 21:56:56 -0500

    feat: Add Firebase configuration and rules for Firestore and Storage

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,17 +2,20 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyBSRcCKLteQMAe9no72GCDReFHWo1xSUQk",
+  authDomain: "catbot-d830b.firebaseapp.com",
+  projectId: "catbot-d830b",
+  storageBucket: "catbot-d830b.firebasestorage.app",
+  messagingSenderId: "551252599022",
+  appId: "1:551252599022:web:aa7ef7b1d25eb4af7713ff",
+  measurementId: "G-EQ0WC5WH12"
 };
 
 const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
